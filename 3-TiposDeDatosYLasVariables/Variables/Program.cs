7901f243ffa9e6e772871d436d0f4afd919838ba
Jose E Garcia <Chawiry>
2025-01-16 18:21:09 -0400

    3rd exercise

---
+++ 3-TiposDeDatosYLasVariables/Variables/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int x;
+            // int y;
+
+            // x = 7;
+            // y = x + 3;
+
+            // Console.WriteLine(y);
+            // Console.ReadLine();
+
+
+            Console.WriteLine( "What is your name?" );
+            Console.Write("First name:");
+            string? myFirstName;
+            myFirstName = Console.ReadLine();
+
+            string? myLastName;
+            Console.Write("Last Name:");
+            myLastName = Console.ReadLine();
+            
+            Console.WriteLine($"Hello, {myFirstName} {myLastName}");
+            Console.ReadLine();
+        }
+    }
+}
