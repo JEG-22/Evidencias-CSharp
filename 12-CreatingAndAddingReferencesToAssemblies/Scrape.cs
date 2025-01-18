27493033441d9f943cd64b1733af14fc5196c35f
Jose E Garcia <Chawiry>
2025-01-17 23:28:47 -0400

    ejercicio 12

---
+++ 12-CreatingAndAddingReferencesToAssemblies/Scrape.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+using System.IO;
+
+namespace _12_CreatingAndAddingReferencesToAssemblies;
+
+class Scrape
+{
+    static void Main(string[] args)
+    {
+        
+    }
+    public string ScrapeWebpage(string url)
+    {
+        return GetWebpage(url);
+    }
+
+    public string ScrapeWebpage(string url, string filepath)
+    {
+        string reply = GetWebpage(url);
+        File.WriteAllText(filepath, reply);
+        return reply;
+
+    }
+    private string GetWebpage(string url)
+    {
+        WebClient client = new WebClient();
+        return client.DownloadString(url);
+    }
+
+
+}
