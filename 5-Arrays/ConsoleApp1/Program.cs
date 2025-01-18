83556d001fcf3e38fe028819fe824f4d25874c1b
Jose E Garcia <Chawiry>
2025-01-17 20:54:06 -0400

    ejercicio 5

---
+++ 5-Arrays/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // int[] numbers = new int[5];
+
+            // numbers[0] = 4;
+            // numbers[1] = 8;
+            // numbers[2] = 15;
+            // numbers[3] = 16;
+            // numbers[4] = 23;
+
+            // // Console.WriteLine(numbers[1]);
+            // Console.WriteLine(numbers.Length);
+
+            // int[] numbers = new int[] {4,8,15,16,23,42};
+
+            string[] names = new string[] {"Eddie", "Jose", "Alex", "Michael"};
+
+            // for (int i = 0; i < names.Length; i++)
+            // {
+            //     Console.WriteLine(names[i]);
+            // }
+
+            // foreach (string name in names)
+            // {
+            //     Console.WriteLine(name);
+            // }
+
+            string zig = "You can get what you want out of life if you help enough other people get what they want.";
+            
+            char[] charArray = zig.ToCharArray();
+            Array.Reverse(charArray);
+
+            foreach (char zigChar in charArray)
+            {
+                Console.Write(zigChar);
+            }
+        }
+    }
+}
