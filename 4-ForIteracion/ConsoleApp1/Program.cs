abf121fe2c2147e5237cad447f36dde6750153ef
Jose E Garcia <Chawiry>
2025-01-17 20:40:29 -0400

    ejercicio 4

---
+++ 4-ForIteracion/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if(i == 7)
+                {
+                    Console.WriteLine("Found Sever!");
+                    break;
+                }
+            }
+
+            for (int myValue = 0; myValue < 12; myValue++)
+            {
+                Console.WriteLine(myValue);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
